b120bde5174465a6b379c46455a5c42d32d1bf9c
Leontine Claassen <leon.claassen00@gmail.com>
2021-06-25 18:07:58 +0200

    Carousel

--- Carousel JS/carousel.js
+++ Carousel JS/carousel.js
@@ -2,8 +2,9 @@
 
 let variableSlides = document.getElementsByClassName('slide');   //contains 3 slides
 let visibleSlide = 0;
-let numberOfSlides = variableSlides.length //makes array like object?
-
+let numberOfSlides = variableSlides.length //there are 3 slides...making sure
+let x = document.getElementById('image1').offsetWidth
+console.log(x);  //but how to use it to move the images?
 
 
 document.getElementById('goRight').addEventListener('click', function(){ //activates right arrow
@@ -17,18 +18,18 @@ document.getElementById('goLeft').addEventListener('click', function() {  //acti
    
 })
 
-moveSlideRight = () => {  //to start image1 is 0 in array like object
-    if(visibleSlide === 0) {  //if 0/image 1 
+moveSlideRight = () => {  
+    if(visibleSlide === 0) {  //if image 1 
 
         visibleSlide++;       //increment
-        document.getElementById("image2").style.left  = ".2%";
         document.getElementById("image1").style.left = "-100%";
-
-    } else if(visibleSlide === 1) {    //if image2 is visible
+        document.getElementById("image2").style.left  =  ".2%";  //glitchy on first move. ???
+    } else if(visibleSlide === 1) {    //if image2 
 
         visibleSlide++;                //increment
-        document.getElementById("image2").style.opacity = "0";  //hide image2 
+        document.getElementById("image2").style.left = "-100%";  //hide image2 
         document.getElementById("image3").style.left = "-.2%";  //make image3 visible
+        
     }
    }
 
@@ -37,17 +38,16 @@ moveSlideLeft = () => {       //function when click left
 
         visibleSlide--;        //decrement
         document.getElementById("image3").style.left = "100%";  // moves image3 back
-        document.getElementById("image2").style.opacity = "1";  //make visible image2 
+        document.getElementById("image2").style.left = ".09%";  //moves image 2
         
     } else if (visibleSlide === 1) {    //if image2 
 
         visibleSlide--;                  //decrement
-        document.getElementById("image2").style.left = "50%"; //moves image 2 back
-        document.getElementById("image1").style.left = ".2%"; //moves image 3 back
+        document.getElementById("image2").style.left = "100%"; //moves image 2 back
+        document.getElementById("image1").style.left = ".2%"; //moves image 1 back
         
     }
 }
 
-let x = document.getElementById('image1').offsetWidth
-console.log(x);  //maybe 
+
 
